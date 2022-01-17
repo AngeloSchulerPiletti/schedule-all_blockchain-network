f93bdb6a04dbb50a7c1ea487dd76bc667b8186aa
Angelo Schuler Piletti <angelo@tideal.com.br>
2022-01-17 14:32:25 -0300

    fixing deployment bug of interfaces

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,11 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 const TaskToken = artifacts.require("TaskToken");
-const ITaskToken = artifacts.require("ITaskToken");
-const IERC20 = artifacts.require("IERC20");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(TaskToken);
-  deployer.deploy(ITaskToken);
-  deployer.deploy(IERC20);
 };
